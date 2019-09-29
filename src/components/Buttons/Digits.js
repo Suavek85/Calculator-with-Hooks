b7ca517f5fb60d0f9e34da0e927e6ec088dd391b
SNiemiec <slawekniemiec@gmail.com>
2019-09-29 21:28:53 +0200

    Add flex order to control flow of useTrail hook

--- src/components/Buttons/Digits.js
+++ src/components/Buttons/Digits.js
@@ -6,29 +6,46 @@ function Digits(props) {
   return (
     <div className="wrapper">
       <Trail
-        items={[9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "decimal"]}
+        //items={[9, 8, 7, 6, 5, 4, 3, 2, 1, 0, "decimal"]}
+        items={[
+          { name: 9, order: 1 },
+          { name: 6, order: 4 },
+          { name: 3, order: 7 },
+          { name: 0, order: 10 },
+          { name: 8, order: 2 },
+          { name: 5, order: 5 },
+          { name: 2, order: 8 },
+          { name: "decimal", order: 11 },
+          { name: 7, order: 3 },
+          { name: 4, order: 6 },
+          { name: 1, order: 9 },
+        ]}
         keys={el => el}
         from={{ opacity: 0 }}
         to={{ opacity: 1 }}
       >
         {el => fadein =>
-          el === "decimal" ? (
+          el.name === "decimal" ? (
             <button
               style={fadein}
               onClick={props.handleSettingInput}
-              key="10"
+              key="11"
               data-foo="."
+              data-order={el.order}
+              className="btn-order"
             >
               .
             </button>
           ) : (
             <button
               onClick={props.handleSettingInput}
-              key={el}
-              data-foo={el}
+              key={el.order}
+              data-order={el.order}
+              data-foo={el.name}
               style={fadein}
+              className="btn-order"
             >
-              {el}
+              {el.name}
             </button>
           )}
       </Trail>
