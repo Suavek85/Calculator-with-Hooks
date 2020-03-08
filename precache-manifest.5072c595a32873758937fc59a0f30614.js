d0c1c3f69b6ee7283098a3f8ef4aecc62f4ec1cd
Slawomir Niemiec <slawekniemiec@gmail.com>
2020-03-08 10:13:44 +0000

    Updates

---
+++ precache-manifest.5072c595a32873758937fc59a0f30614.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4f3612722424cbf8f277a7b8eeb0ec",
+    "url": "/Calculator-with-Hooks/index.html"
+  },
+  {
+    "revision": "a6f61228f3387299eea3",
+    "url": "/Calculator-with-Hooks/static/css/main.8a81bf10.chunk.css"
+  },
+  {
+    "revision": "598b28310acf42c439b1",
+    "url": "/Calculator-with-Hooks/static/js/2.dfe7e506.chunk.js"
+  },
+  {
+    "revision": "a6f61228f3387299eea3",
+    "url": "/Calculator-with-Hooks/static/js/main.40d6cc64.chunk.js"
+  },
+  {
+    "revision": "08a075b44f113c6c62b7",
+    "url": "/Calculator-with-Hooks/static/js/runtime-main.0f590cb7.js"
+  }
+]);
